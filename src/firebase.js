9f0333e6503e881a5fec063c670ebc5988309810
04zAM <49489566+04zAM@users.noreply.github.com>
2022-01-09 13:44:04 -0500

    dotenv implementation

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBYfbDKWA9j5uWoooBeq_4yjM15tQViTnY",
-    authDomain: "clone-1708c.firebaseapp.com",
-    projectId: "clone-1708c",
-    storageBucket: "clone-1708c.appspot.com",
-    messagingSenderId: "383897857096",
-    appId: "1:383897857096:web:d073b404b09b2aa7b9bc7d",
-    measurementId: "G-XZ8S6M1RZJ"
-  };
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
